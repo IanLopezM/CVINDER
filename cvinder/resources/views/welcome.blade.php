58a57111989b8e7a26162a40372c7f0aa3079236
IanLopezM <72654073+IanLopezM@users.noreply.github.com>
2022-04-28 21:41:07 +0200

    welcome.blade.php
    xd

--- cvinder/resources/views/welcome.blade.php
+++ cvinder/resources/views/welcome.blade.php
@@ -18,118 +18,56 @@
             body {
                 font-family: 'Nunito', sans-serif;
             }
-            nav {
-  margin: 27px auto 0;
-
-  position: relative;
-  width: 590px;
-  height: 50px;
-  background-color: #34495e;
-  border-radius: 8px;
-  font-size: 0;
-}
-nav a {
-  line-height: 50px;
-  height: 100%;
-  font-size: 15px;
-  display: inline-block;
-  position: relative;
-  z-index: 1;
-  text-decoration: none;
-  text-transform: uppercase;
-  text-align: center;
-  color: white;
-  cursor: pointer;
-}
-nav .animation {
-  position: absolute;
-  height: 100%;
-  top: 0;
-  z-index: 0;
-  transition: all .5s ease 0s;
-  border-radius: 8px;
-}
-a:nth-child(1) {
-  width: 100px;
-}
-a:nth-child(2) {
-  width: 110px;
-}
-a:nth-child(3) {
-  width: 100px;
-}
-a:nth-child(4) {
-  width: 160px;
-}
-a:nth-child(5) {
-  width: 120px;
-}
-nav .start-home, a:nth-child(1):hover~.animation {
-  width: 100px;
-  left: 0;
-  background-color: #1abc9c;
-}
-nav .start-about, a:nth-child(2):hover~.animation {
-  width: 110px;
-  left: 100px;
-  background-color: #e74c3c;
-}
-nav .start-blog, a:nth-child(3):hover~.animation {
-  width: 100px;
-  left: 210px;
-  background-color: #3498db;
-}
-nav .start-portefolio, a:nth-child(4):hover~.animation {
-  width: 160px;
-  left: 310px;
-  background-color: #9b59b6;
-}
-nav .start-contact, a:nth-child(5):hover~.animation {
-  width: 120px;
-  left: 470px;
-  background-color: #e67e22;
-}
-
-body {
-  font-size: 12px;
-  font-family: sans-serif;
-  background: #2c3e50;
-}
-h1 {
-  text-align: center;
-  margin: 40px 0 40px;
-  text-align: center;
-  font-size: 30px;
-  color: #ecf0f1;
-  text-shadow: 2px 2px 4px #000000;
-  font-family: 'Cherry Swash', cursive;
-}
-
-p {
-    position: absolute;
-    bottom: 20px;
-    width: 100%;
-    text-align: center;
-    color: #ecf0f1;
-    font-family: 'Cherry Swash',cursive;
-    font-size: 16px;
-}
-
-span {
-    color: #2BD6B4;
-}
+            .split {
+              height: 100%;
+              width: 50%;
+              position: fixed;
+              z-index: 1;
+              top: 0;
+              overflow-x: hidden;
+              padding-top: 20px;
+            }
+            .left {
+              left: 0;
+              background: rgb(129,212,255);
+              background: linear-gradient(180deg, rgba(129,212,255,1) 0%, rgba(159,198,255,1) 100%);
+            }
+            .right {
+              right: 0;
+              background: rgb(255,179,70);
+              background: linear-gradient(180deg, rgba(255,179,70,1) 0%, rgba(255,216,159,1) 100%);
+            }
+            .centered {
+              position: absolute;
+              top: 50%;
+              left: 50%;
+              transform: translate(-50%, -50%);
+              text-align: center;
+            }
+            .centered img {
+              width: 150px;
+              border-radius: 50%;
+            }
         </style>
     </head>
     <body class="antialiased">
-        <nav>
-            <a href="#">Home</a>
-            <a href="#">About</a>
-            <a href="#">Blog</a>
-            <a href="#">Portefolio</a>
-            <a href="#">Contact</a>
-        <div class="animation start-home"></div>
-        </nav>
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+    <div class="split left">
+      <div class="centered">
+        <h2>Empresa</h2>
+        <!-- <p><a href="{{route('enterprise.form')}}">Inicia sesión</a></p> -->
+        <p><a href="{{route('enterprise.form')}}">Crea una cuenta de empresa</a></p>
+      </div>
+    </div>
+
+    <div class="split right">
+      <div class="centered">
+        <h2>Trabajador</h2>
+        <!-- <p><a href="{{route('worker.form')}}">Inicia sesión</a></p> -->
+        <p><a href="{{route('worker.form')}}">Crea una cuenta de trabajador</a></p>
+      </div>
+    </div>
+
+        <!-- <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
@@ -143,6 +81,6 @@ span {
                     @endauth
                 </div>
             @endif
-        </div>
+        </div> -->
     </body>
 </html>
--- cvinder/routes/web.php
+++ cvinder/routes/web.php
@@ -34,9 +34,12 @@ Route::middleware([
 Route::get("/worker", [WorkerController::class, "index"])->name("worker.index");
 Route::get("/worker/profile", [WorkerController::class, "profile"])->name("worker.profile");
 Route::get("/worker/matches", [WorkerController::class, "matches"])->name("worker.matches");
+Route::get("/worker/form", [WorkerController::class, "form"])->name("worker.form");
 
-Route::get("/enterprise", [EnterpriseController::class, "profile"])->name("enterprise.profile");
+Route::get("/enterprise/profile", [EnterpriseController::class, "profile"])->name("enterprise.profile");
+Route::get("/enterprise/form", [EnterpriseController::class, "form"])->name("enterprise.form");
 
 Route::get("/offer", [OfferController::class, "index"])->name("offer.index");
 Route::get("/offer/matches", [OfferController::class, "matches"])->name("offer.matches");
+Route::get("/offer/form", [OfferController::class, "form"])->name("offer.form");
 
