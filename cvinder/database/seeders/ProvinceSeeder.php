06e6637ff339a1368dcc0def4606504e2c494a7f
projecto3m12 <102583626+projecto3m12@users.noreply.github.com>
2022-05-11 02:25:26 +0200

    province seeder

--- cvinder/database/seeders/ProvinceSeeder.php
+++ cvinder/database/seeders/ProvinceSeeder.php
@@ -14,12 +14,14 @@ class ProvinceSeeder extends Seeder
      */
     public function run()
     {
-        $provincias = ['Alava','Albacete','Alicante','Almería','Asturias','Avila','Badajoz','Barcelona','Burgos','Cáceres',
-        'Cádiz','Cantabria','Castellón','Ciudad Real','Córdoba','La Coruña','Cuenca','Gerona','Granada','Guadalajara',
-        'Guipúzcoa','Huelva','Huesca','Islas Baleares','Jaén','León','Lérida','Lugo','Madrid','Málaga','Murcia','Navarra',
-        'Orense','Palencia','Las Palmas','Pontevedra','La Rioja','Salamanca','Segovia','Sevilla','Soria','Tarragona',
-        'Santa Cruz de Tenerife','Teruel','Toledo','Valencia','Valladolid','Vizcaya','Zamora','Zaragoza'];
-        
+        $provincias = [
+            'Alava', 'Albacete', 'Alicante', 'Almería', 'Asturias', 'Avila', 'Badajoz', 'Barcelona', 'Burgos', 'Cáceres',
+            'Cádiz', 'Cantabria', 'Castellón', 'Ciudad Real', 'Córdoba', 'La Coruña', 'Cuenca', 'Gerona', 'Granada', 'Guadalajara',
+            'Guipúzcoa', 'Huelva', 'Huesca', 'Islas Baleares', 'Jaén', 'León', 'Lérida', 'Lugo', 'Madrid', 'Málaga', 'Murcia', 'Navarra',
+            'Orense', 'Palencia', 'Las Palmas', 'Pontevedra', 'La Rioja', 'Salamanca', 'Segovia', 'Sevilla', 'Soria', 'Tarragona',
+            'Santa Cruz de Tenerife', 'Teruel', 'Toledo', 'Valencia', 'Valladolid', 'Vizcaya', 'Zamora', 'Zaragoza'
+        ];
+
 
         for ($i = 0; $i < count($provincias); $i++) {
             Province::create([
--- cvinder/database/seeders/SkillSeeder.php
+++ cvinder/database/seeders/SkillSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Skill;
 
 class SkillSeeder extends Seeder
 {
@@ -13,6 +14,24 @@ class SkillSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $skills = [
+            'C', 'C++', 'C#', 'Go', 'Java', 'JavaScript', 'TypeScript', 'PHP', 'Clojure', 'CoffeeScript', 'Elixir',
+            'Erlang', 'Haskell', 'Nim', 'Perl', 'Python', 'Ruby', 'Rust', 'Scala', 'Swift', 'Angular', 'Babel', 'Angularjs',
+            'Bakcbonejs', 'Bootstrap', 'Tailwind', 'Bulma', 'CSS3', 'Ember', 'Gulp', 'HTML5', 'Materialize', 'Pug', 'Qt', 'React',
+            'Redux', 'Sass', 'Svelte', 'Vuejs', 'Vuetify', 'Webpack', 'Express', 'Graphql', 'Hadoop', 'Kafka', 'Nestjs',
+            'Nginx', 'Nodejs', 'Openresty', 'RabbitMQ', 'Spring', 'Android', 'Apachecordova', 'Dart', 'Flutter', 'Ionic', 'Kotlin',
+            'Nativescript', 'Reactnative', 'Xamarin', 'Cassandra', 'Cockroachdb', 'Couchdb', 'Elasticsearch', 'Hive',
+            'Mariadb', 'Mongodb', 'Mssql', 'Mysql', 'Opencv', 'Oracle', 'Pandas', 'PostgreSQL', 'Pytorch', 'Realm', 'Redis',
+            'Sqlite', 'AWS', 'Azure', 'Bash', 'Canvasjs', 'Chartjs', 'Docker', 'Grafana', 'Jenkins', 'Kibana', 'Kubernetes',
+            'Vagrant', 'Amplify', 'Appwrite', 'Arduino', 'Codeigniter', 'Django', 'Dotnet', 'Electron', 'Firebase', 'Flask',
+            'Git', 'Heroku', 'Ifttt', 'Laravel', 'Linux', 'Windows', 'Mocha', 'Postman', 'Quasar', 'Rails', 'Symfony', 'Unity',
+            'Unreal', 'Zapier'
+        ];
+
+        for ($i = 0; $i < count($skills); $i++) {
+            Skill::create([
+                'name' => $skills[$i],
+            ]);
+        }
     }
 }
